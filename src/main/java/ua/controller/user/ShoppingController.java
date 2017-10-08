23d5df367ba5e11acb7aa5a7b2560b129134b067
Mykola <bodnar.mykola1997@gmail.com>
2017-10-08 16:12:11 +0300

    cookie

--- src/main/java/ua/controller/user/ShoppingController.java
+++ src/main/java/ua/controller/user/ShoppingController.java
@@ -1,6 +1,5 @@
 package ua.controller.user;
 
-import com.sun.deploy.net.HttpResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
